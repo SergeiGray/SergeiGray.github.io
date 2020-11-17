d57f6ae0da1daee5d96175f8bc116d250b55958b
SergeiGray <borodat_for@mail.ru>
2020-11-17 21:23:28 +0300

    add new site

---
+++ assets/projects/netflix-roulette/precache-manifest.3641b5a21dc44391ff821082c7062902.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a70677543ce342a3810c5a0c12a7747",
+    "url": "index.html"
+  },
+  {
+    "revision": "336790060d13fe46bf4d",
+    "url": "static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "76a375c627e99160fbc7",
+    "url": "static/css/main.7abf4298.chunk.css"
+  },
+  {
+    "revision": "336790060d13fe46bf4d",
+    "url": "static/js/2.7f830ee0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "static/js/2.7f830ee0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76a375c627e99160fbc7",
+    "url": "static/js/main.840a552f.chunk.js"
+  },
+  {
+    "revision": "fe2af3bd6869259f6b81",
+    "url": "static/js/runtime-main.49925162.js"
+  },
+  {
+    "revision": "f4c93e0b8977bb621f6fb11e412ea8e7",
+    "url": "static/media/background.f4c93e0b.jpg"
+  },
+  {
+    "revision": "71c4a61658870f5f7d239e3414f35e42",
+    "url": "static/media/countdown.71c4a616.png"
+  }
+]);
