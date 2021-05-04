151c9fb7eae265e3d8667d8e6592a955152159ee
SergeiGray <borodat_for@mail.ru>
2021-05-04 14:30:43 +0300

    stable version returned

---
+++ assets/projects/netflix-roulette/precache-manifest.67bba939d451154725f24e9ffac185bf.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a53150acd9d4fb58e1957ee341e69a72",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d52d2860bd0352f98599",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "48f1320c4ba80b53b55c",
+    "url": "/static/css/main.7abf4298.chunk.css"
+  },
+  {
+    "revision": "d52d2860bd0352f98599",
+    "url": "/static/js/2.b8b2794e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b8b2794e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48f1320c4ba80b53b55c",
+    "url": "/static/js/main.53a1c235.chunk.js"
+  },
+  {
+    "revision": "fe2af3bd6869259f6b81",
+    "url": "/static/js/runtime-main.49925162.js"
+  },
+  {
+    "revision": "f4c93e0b8977bb621f6fb11e412ea8e7",
+    "url": "/static/media/background.f4c93e0b.jpg"
+  },
+  {
+    "revision": "71c4a61658870f5f7d239e3414f35e42",
+    "url": "/static/media/countdown.71c4a616.png"
+  }
+]);
